252b01b5da9359e1b11f94329d9a9d4ec90a0449
Kiyoshika <zachlweaver00@gmail.com>
2025-12-26 23:21:29 -0800

    update TODO comments

--- SyntaxHighlighter.cpp
+++ SyntaxHighlighter.cpp
@@ -5,6 +5,7 @@ using namespace openide;
 SyntaxHighlighter::SyntaxHighlighter(QTextDocument* parent) : QSyntaxHighlighter(parent), m_keywords{{}}
 {
     // TODO: add support for theme/styles at some point
+    // need to support major/minor keywords, comments, string/char/numeric literals, etc.
     m_keywordFormat.setForeground(Qt::blue);
     m_keywordFormat.setFontWeight(QFont::Bold);
 }
@@ -23,8 +24,8 @@ void SyntaxHighlighter::highlightBlock(const QString& text)
 {
     if (m_keywords.length() == 0) return;
 
-    // TODO: test performance of regex on large files;
-    // otherwise use a different tokenization method
+    // TODO: replace this with TreeSitter; this is just a placeholder
+    // to have some level of highlighting
     for (const QString& word : m_keywords)
     {
         QRegularExpression re("\\b" + QRegularExpression::escape(word) + "\\b");
